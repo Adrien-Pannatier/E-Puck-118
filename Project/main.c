4e5c8843b8b9220c437a2617bf6c624fe1d4ac18
Adrien-Pannatier <adrien.pannatier@epfl.ch>
2022-04-06 19:19:32 +0200

    Initial Push

---
+++ Project/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
+#include <chprintf.h>
+#include <motors.h>
+#include <audio/microphone.h>
+
+int main(void)
+{
+    /* Infinite loop. */
+    while (1) {
+
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
