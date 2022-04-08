46e0e06bb7948bd71249f847fbcfe908c11ba7bc
Adrien-Pannatier <adrien.pannatier@epfl.ch>
2022-04-08 11:40:03 +0200

    Added rocky song and tempo

--- project_files/added_melodies.c
+++ project_files/added_melodies.c
@@ -9,56 +9,142 @@
 
 //ROCKY melody
 static const uint16_t rocky_melody[] = {
-    NOTE_G4, 0, 0, NOTE_G4,
-    0, 0, NOTE_AS4, 0 ,
-    NOTE_C5, 0, NOTE_G4, 0,
-    0, NOTE_G4, 0, 0,
-    NOTE_F4, 0, NOTE_FS4, 0,
+	0, 0, 0, NOTE_C4,
 
-    NOTE_G4, 0, 0, NOTE_G4,
-    0, 0, NOTE_AS4, 0 ,
-    NOTE_C5, 0, NOTE_G4, 0,
-    0, NOTE_G4, 0, 0,
-    NOTE_F4, 0, NOTE_FS4, 0,
+	NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4,
+	NOTE_C4, NOTE_C4, NOTE_E4, NOTE_C4,
+	NOTE_C4, NOTE_C4,NOTE_E4,
 
-    NOTE_AS5, NOTE_G5, NOTE_D5,0,
-    0,0,0,0,
-    0,0, NOTE_AS5, NOTE_G5,
-    NOTE_CS5,0, 0, 0,
-    0, 0, 0, 0,
-    NOTE_AS5, NOTE_G5, NOTE_C5,0,
-    0, 0, 0, 0,
-    0, 0, NOTE_AS4, NOTE_C5,
-    0, 0, 0, 0,
-    0, 0, 0, 0
+	NOTE_E4, NOTE_E4, NOTE_E4, NOTE_E4,
+	NOTE_E4, NOTE_E4, NOTE_G4, NOTE_E4,
+	NOTE_E4, NOTE_E4,
 
+	NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4,
+	NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4,
+	NOTE_D4, 0, NOTE_D4,
+
+	NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4,
+	NOTE_D4, NOTE_D4, NOTE_D4, NOTE_E4,
+	NOTE_G4,
+
+	//REFRAIN
+	NOTE_A5, NOTE_A5, NOTE_B5,
+
+	NOTE_E4, NOTE_E4, NOTE_G4,
+
+	NOTE_A5, NOTE_A5, NOTE_B5,
+
+	NOTE_E4,
+
+
+	NOTE_C4, NOTE_D4, NOTE_C4, NOTE_D4,
+	NOTE_C4, NOTE_D4, NOTE_E4,
+
+	NOTE_A4, NOTE_C4, NOTE_C4, NOTE_B4,
+	NOTE_B4, NOTE_A4, NOTE_B4, NOTE_B4,
+	NOTE_F4,
+
+	NOTE_E4, NOTE_E4, NOTE_E4, NOTE_E4,
+	NOTE_E4, NOTE_E4,
+
+	NOTE_E4, NOTE_E4, NOTE_E4, NOTE_E4,
+	NOTE_E4, NOTE_E4, NOTE_E4, NOTE_E4,
+	NOTE_E4, NOTE_G4,
+
+	//REFRAIN
+	NOTE_A5, NOTE_A5, NOTE_B5,
+
+	NOTE_E4, NOTE_E4, NOTE_G4,
+
+	NOTE_A5, NOTE_A5, NOTE_B5,
+
+	NOTE_E4,
+
+
+	NOTE_C4, NOTE_D4, NOTE_C4, NOTE_D4,
+	NOTE_C4, NOTE_D4, NOTE_E4,
+
+	NOTE_E4, NOTE_C5, NOTE_C5, NOTE_B5,
+	NOTE_B5, NOTE_A5, NOTE_A5, NOTE_A5,
+	NOTE_G4, NOTE_F5,
+
+	NOTE_E5, NOTE_E4, NOTE_E4, NOTE_E4,
+	NOTE_E4, NOTE_E4, NOTE_E4,
+
+	NOTE_E4, NOTE_E4, NOTE_E4, NOTE_E4,
+	NOTE_E4, NOTE_E4, NOTE_G4,
+
+	NOTE_A5, NOTE_G4, NOTE_A5, NOTE_C5,
+
+	NOTE_A5,
 };
 
 //ROCKY tempo
 static const float rocky_tempo[] = {
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
+	1/2, 1/4, 1/8, 1/8,
 
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
+	1/8, 1/16, 1/16, 1/8,
+	1/16, 1/16, 1/8, 1/16,
+	1/16, 1/8, 1/8,
 
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
+	1/8, 1/16, 1/16, 1/8,
+	1/16, 1/16, 1/8, 1/16,
+	1/16, 1/4,
 
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12,
-      12, 12, 12, 12
+	1/8, 1/16, 1/16, 1/8,
+	1/16, 1/16, 1/16, 1/16,
+	1/8, 1/8, 1/8,
+
+	1/8, 1/16, 1/16, 1/16,
+	1/8, 1/16, 1/4, 1/16,
+	3/16,
+
+	3/4, 1/16, 3/16,
+
+	3/4, 1/16, 3/16,
+
+	3/4, 1/16, 3/16,
+
+	1,
+
+	1/8, 1/16, 1/16, 3/16,
+	1/16, 1/16, 7/16,
+
+	1/8, 1/16, 1/16, 1/8,
+	1/16, 2/16, 1/16, 1/4,
+	1/8,
+
+	1/4, 1/4, 1/8, 1/8,
+	1/8, 1/8,
+
+	1/16, 1/8, 1/16, 1/8,
+	1/16, 2/16, 1/16, 1/8,
+	1/16, 3/16,
+
+	3/4, 1/16, 3/16,
+
+	3/4, 1/16, 3/16,
+
+	3/4, 1/16, 3/16,
+
+	1,
+
+	1/8, 1/16, 1/16, 3/16,
+	1/16, 1/16, 7/16,
+
+	1/8, 1/16, 1/16, 1/8,
+	1/16, 1/16, 1/16, 1/16,
+	1/4, 1/8,
+
+	1/4, 1/8, 1/8, 1/8,
+	1/8, 1/8, 1/8,
+
+	1/8, 1/8, 1/8, 1/8,
+	1/4, 1/16, 3/16,
+
+	1/4, 1/16, 3/16, 1/4,
+
+	5/4,
 
 };
 
@@ -68,8 +154,11 @@ static const melody_t added_melodies[NB_ADDED_SONGS] = {
     .notes = rocky_melody,
     .tempo = rocky_tempo,
     .length = sizeof(rocky_melody)/sizeof(uint16_t),
-
   },
 };
 
+void playAddedMelody(added_song_selection_t choice, play_melody_option_t option, float tempo_scale){
+	choice.tempo = choice.tempo * tempo_scale;
+	playMelody(EXTERNAL_SONG, option, choice);
+}
 
--- project_files/added_melodies.h
+++ project_files/added_melodies.h
@@ -17,4 +17,17 @@ typedef enum{
 	NB_ADDED_SONGS,	//tell the number of internal songs
 }added_song_selection_t;
 
+
+/**
+ * @brief Plays the selected melody from the external library. Does nothing if the module has not been
+ * 							 started with playMelodyStart()	This function doesn't block the current thread.
+ * 							 It uses it's self thread
+ *
+ * @param choice 			Song selected (see added_song_selection_t)
+ * @param option			Behavior to change the melody playing (see play_melody_option_t)
+ * @param tempo_scale		Used to scale the program tempo of the melody.
+ *
+ */
+void playAddedMelody(added_song_selection_t choice, play_melody_option_t option, float tempo_scale);
+
 #endif /* ADDED_MELODIES_H_ */
