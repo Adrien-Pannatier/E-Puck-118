980c29e842f795214394a337b6a00265efa668d8
APraplan <axel.praplan@epfl.ch>
2022-04-12 17:37:27 +0200

    light gestion creation

--- project_files/main.c
+++ project_files/main.c
@@ -4,6 +4,7 @@
 
 #include "ch.h"
 #include "hal.h"
+#include "spi_comm.h"
 #include "memory_protection.h"
 #include <usbcfg.h>
 #include <main.h>
@@ -14,64 +15,26 @@
 #include <msgbus/messagebus.h>
 #include <audio/play_melody.h>
 #include <added_melodies.h>
+#include "chmtx.h"
+#include "light_gestion.h"
 
-messagebus_t bus;
-MUTEX_DECL(bus_lock);
-CONDVAR_DECL(bus_condvar);
-
-//static THD_WORKING_AREA(waThdFrontLed, 1024);
-//static THD_FUNCTION(ThdFrontLed, arg) {
-//
-//    chRegSetThreadName(__FUNCTION__);
-//    (void)arg;
-//
-//    systime_t time;
-//
-//    while(1){
-//    	playMelody(MARIO,ML_WAIT_AND_CHANGE,0);
-//    }
-//}
 
 int main(void)
 {
     halInit();
     chSysInit();
     mpu_init();
-    messagebus_init(&bus, &bus_lock, &bus_condvar);
-    proximity_start();
-    spi_comm_start();
-    playMelodyStart();
 
-    // chThdCreateStatic(waThdFrontLed, sizeof(waThdFrontLed), NORMALPRIO, ThdFrontLed, NULL);
+    LED_start();
 
     /* Infinite loop. */
     while (1) {
+    	chThdSleepMilliseconds(300);
+    	if(get_selector() == 8){
+    		stopCurrentMelody();
+    	}
 
-//    	playMelody(ML_SIMPLE_PLAY,ML_WAIT_AND_CHANGE,NULL);
-//    	waitMelodyHasFinished();
-
-    	toggle_rgb_led(LED2, RED_LED,0);
-        toggle_rgb_led(LED4, RED_LED,0);
-    	toggle_rgb_led(LED6, RED_LED,0);
-        toggle_rgb_led(LED8, RED_LED,0);
-    	toggle_rgb_led(LED2, BLUE_LED,100);
-    	toggle_rgb_led(LED4, BLUE_LED,100);
-    	toggle_rgb_led(LED6, BLUE_LED,100);
-    	toggle_rgb_led(LED8, BLUE_LED,100);
-
-    	chThdSleepMilliseconds(500);
-
-    	toggle_rgb_led(LED2, BLUE_LED,0);
-    	toggle_rgb_led(LED4, BLUE_LED,0);
-    	toggle_rgb_led(LED6, BLUE_LED,0);
-    	toggle_rgb_led(LED8, BLUE_LED,0);
-    	toggle_rgb_led(LED2, RED_LED,100);
-    	toggle_rgb_led(LED4, RED_LED,100);
-    	toggle_rgb_led(LED6, RED_LED,100);
-    	toggle_rgb_led(LED8, RED_LED,100);
-    	chThdSleepMilliseconds(500);
-
-    }
+    	}
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
