1a3af72acd1468ecbb463b18aaf743b6ebe8cac0
Adrien-Pannatier <adrien.pannatier@epfl.ch>
2022-04-12 17:38:32 +0200

    melodies modification

--- project_files/added_melodies.h
+++ project_files/added_melodies.h
@@ -27,6 +27,6 @@ typedef enum{
  * @param option			Behavior to change the melody playing (see play_melody_option_t)
  *
  */
-void playAddedMelody(added_song_selection_t choice, play_melody_option_t option);
+void playAddedMelody(added_song_selection_t choice, play_melody_option_t option, uint16_t tempo_music);
 
 #endif /* ADDED_MELODIES_H_ */
