da45a421eb2d738dc1d4335196188175d2f2a597
APraplan <axel.praplan@epfl.ch>
2022-04-14 12:11:14 +0200

    IR setup

--- project_files/management_proximity.c
+++ project_files/management_proximity.c
@@ -5,24 +5,72 @@
  *      Author: APrap
  */
 
-#include <proximity.h>
+#include <management_proximity.h>
 #include <msgbus/messagebus.h>
+#include "ch.h"
+#include "hal.h"
+#include "leds.h"
+#include <stm32f4xx.h>
+#include "sensors/proximity.h"
+#include <usbcfg.h>
+#include "chprintf.h"
 
-static THD_WORKING_AREA(waThdProximity, 1024);
-static THD_FUNCTION(ThdProximity, arg) {
+//messagebus_t bus;
+//static messagebus_t bus;
+//static MUTEX_DECL(IR_lock);
+//static CONDVAR_DECL(IR_condvar);
 
-    chRegSetThreadName(__FUNCTION__);
+static THD_WORKING_AREA(waThdManagementProximity, 512);
+static THD_FUNCTION(ThdManagementProximity, arg) {
+
+    chRegSetThreadName("ManagementProximityThd");
     (void)arg;
 
-    //inititalisation proximity sensors
-    proximity_start();
-    messagebus_init(&bus, &bus_lock, &bus_condvar);
+    //Sensor clock init
 
-    systime_t time;
+    RCC->AHB1ENR    |= RCC_AHB1ENR_GPIOBEN;
+    RCC->AHB1ENR    |= RCC_AHB1ENR_GPIOCEN;
+
+    //Start proximity sensors
+    proximity_start();
+    calibrate_ir();
+
+    //Realterm
+    usb_start();
+    serial_start();
+
+	//Sensor value
+    uint16_t tab_prox[PROXIMITY_NB_CHANNELS] = {0};
 
     while(1){
-    	chThdSleepMilliseconds(500);
+
+    	for(int i = 0; i < PROXIMITY_NB_CHANNELS; i++){
+    		tab_prox[i] = get_calibrated_prox(i);
+    	}
+
+    	chprintf((BaseSequentialStream *)&SD3, "%u \n\n\r", tab_prox[0]);
+
+    	chThdSleepMilliseconds(50);
+
     }
 }
 
+void management_proximity_start(void){
+	chThdCreateStatic(waThdManagementProximity, sizeof(waThdManagementProximity), NORMALPRIO, ThdManagementProximity, NULL);
+}
+
+
+// COM
+
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
 
---
+++ project_files/management_proximity.h
@@ -0,0 +1,26 @@
+/*
+ * Proximity.h
+ *
+ *  Created on: 8 avr. 2022
+ *      Author: APrap
+ */
+
+#ifndef PROXIMITY_H_
+#define PROXIMITY_H_
+
+#define IR1		0
+#define IR2		1
+#define IR3		2
+#define IR4		3
+#define IR5		4
+#define IR6		5
+#define IR7		6
+#define IR8		7
+
+void management_proximity_start(void);
+
+//COM
+static void serial_start(void);
+
+
+#endif /* PROXIMITY_H_ */
