0513caac2dbc1d65829b01b09e269cb810b7c078
Adrien-Pannatier <adrien.pannatier@epfl.ch>
2022-04-14 10:38:30 +0200

    serial port communication setup

--- project_files/main.c
+++ project_files/main.c
@@ -24,6 +24,25 @@ messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
+void SendUint8ToComputer(uint8_t* data, uint16_t size)
+{
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
+}
+
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
+
 //static THD_WORKING_AREA(waThdFrontLed, 1024);
 //static THD_FUNCTION(ThdFrontLed, arg) {
 //
