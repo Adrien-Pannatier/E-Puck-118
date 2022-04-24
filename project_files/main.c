4b7e9df7f2ffc745bb09a73745c5a1db664b995a
Adrien-Pannatier <adrien.pannatier@epfl.ch>
2022-04-24 15:17:33 +0200

    merge rectifications

--- project_files/main.c
+++ project_files/main.c
@@ -30,25 +30,6 @@ messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
-void SendUint8ToComputer(uint8_t* data, uint16_t size)
-{
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
-}
-
-static void serial_start(void)
-{
-	static SerialConfig ser_cfg = {
-	    115200,
-	    0,
-	    0,
-	    0,
-	};
-
-	sdStart(&SD3, &ser_cfg); // UART3.
-}
-
 int main(void)
 {
     halInit();
@@ -61,8 +42,6 @@ int main(void)
     dac_start();
     playMelodyStart();
     //starts the camera
-    //starts the serial communication
-    serial_start();
     //start the USB communication
     dcmi_start();
     po8030_start();
